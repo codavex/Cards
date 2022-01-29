eaa7fa2fe82ed838a2adf0d61784cd811315f77f
David Cox <dave@codavex.co.uk>
2022-01-29 22:16:44 +0000

    tidy

--- test/testCard.py
+++ test/testCard.py
@@ -6,33 +6,55 @@ from Suit import Suit
 
 class testCard(unittest.TestCase):
 
+    def setUp(self):
+        self.s = Suit.S
+        self.c = Suit.C
+        self.h = Suit.H
+        self.d = Suit.D
+
+        self.two = Rank(2)
+        self.three = Rank(3)
+
+        self.j = Rank(11)
+        self.q = Rank(12)
+        self.k = Rank(13)
+        self.a = Rank(14)
+
     def test_card_repr(self):
-        card = Card(Rank(2), Suit.S)
+        card = Card(self.two, self.s)
         self.assertEqual(repr(card), "2S")
-        card = Card(Rank(11), Suit.S)
+
+        card = Card(self.j, self.s)
         self.assertEqual(repr(card), "JS")
-        card = Card(Rank(12), Suit.C)
+
+        card = Card(self.q, self.c)
         self.assertEqual(repr(card), "QC")
-        card = Card(Rank(13), Suit.H)
+
+        card = Card(self.k, self.h)
         self.assertEqual(repr(card), "KH")
-        card = Card(Rank(14), Suit.D)
+
+        card = Card(self.a, self.d)
         self.assertEqual(repr(card), "AD")
 
     def test_card_str(self):
-        card = Card(Rank(2), Suit.S)
+        card = Card(self.two, self.s)
         self.assertEqual(str(card), "2 of Spades")
-        card = Card(Rank(11), Suit.S)
+
+        card = Card(self.j, self.s)
         self.assertEqual(str(card), "Jack of Spades")
-        card = Card(Rank(12), Suit.C)
+
+        card = Card(self.q, self.c)
         self.assertEqual(str(card), "Queen of Clubs")
-        card = Card(Rank(13), Suit.H)
+
+        card = Card(self.k, self.h)
         self.assertEqual(str(card), "King of Hearts")
-        card = Card(Rank(14), Suit.D)
+
+        card = Card(self.a, self.d)
         self.assertEqual(str(card), "Ace of Diamonds")
 
     def test_card_gt(self):
-        card1 = Card(Rank(2), Suit.C)
-        card2 = Card(Rank(3), Suit.C)
+        card1 = Card(self.two, self.c)
+        card2 = Card(self.three, self.c)
         self.assertTrue(card2 > card1)
         self.assertTrue(card1 < card2)
         self.assertTrue(card1 != card2)
