fe4ea7d2810f046f6782c1cd488d340f51326088
David Cox <dave@codavex.co.uk>
2022-12-23 13:36:13 +0000

    parameterise testCard.py

--- test/Card/testCard.py
+++ test/Card/testCard.py
@@ -15,41 +15,70 @@ class testCard(unittest.TestCase):
         self.card_ad = Card(Rank(14), Suit.D)
 
     def test_card_repr(self):
-        self.assertEqual("2S", repr(self.card_2s))
-        self.assertEqual("JS", repr(self.card_js))
-        self.assertEqual("QC", repr(self.card_qc))
-        self.assertEqual("KH", repr(self.card_kh))
-        self.assertEqual("AD", repr(self.card_ad))
+        test_cases = (
+            ("2S", self.card_2s),
+            ("JS", self.card_js),
+            ("QC", self.card_qc),
+            ("KH", self.card_kh),
+            ("AD", self.card_ad)
+        )
+
+        for expected_repr, test_instance in test_cases:
+            with self.subTest(f"{expected_repr} == repr({test_instance})"):
+                self.assertEqual(expected_repr, repr(test_instance))
 
     def test_card_str(self):
-        self.assertEqual("2S", str(self.card_2s))
-        self.assertEqual("JS", str(self.card_js))
-        self.assertEqual("QC", str(self.card_qc))
-        self.assertEqual("KH", str(self.card_kh))
-        self.assertEqual("AD", str(self.card_ad))
+        test_cases = (
+            ("2S", self.card_2s),
+            ("JS", self.card_js),
+            ("QC", self.card_qc),
+            ("KH", self.card_kh),
+            ("AD", self.card_ad)
+        )
+
+        for expected_repr, test_instance in test_cases:
+            with self.subTest(f"{expected_repr} == str({test_instance})"):
+                self.assertEqual(expected_repr, str(test_instance))
 
     def test_card_gt(self):
-        self.assertTrue(self.card_ad > self.card_kh)
-        self.assertTrue(self.card_kh > self.card_qc)
-        self.assertTrue(self.card_qc > self.card_js)
-        self.assertTrue(self.card_js > self.card_2s)
+        test_cases = (
+            (self.card_ad, self.card_kh),
+            (self.card_kh, self.card_qc),
+            (self.card_qc, self.card_js),
+            (self.card_js, self.card_2s),
+        )
+
+        for instance_a, instance_b in test_cases:
+            with self.subTest(f"{instance_a} > {instance_b}"):
+                self.assertTrue(instance_a > instance_b)
 
     def test_card_ge(self):
-        self.assertTrue(self.card_ad >= self.card_ad)
-        self.assertTrue(self.card_ad >= self.card_kh)
-        self.assertTrue(self.card_kh >= self.card_kh)
-        self.assertTrue(self.card_kh >= self.card_qc)
-        self.assertTrue(self.card_qc >= self.card_qc)
-        self.assertTrue(self.card_qc >= self.card_js)
-        self.assertTrue(self.card_js >= self.card_js)
-        self.assertTrue(self.card_js >= self.card_2s)
-        self.assertTrue(self.card_2s >= self.card_2s)
+        test_cases = (
+            (self.card_ad, self.card_ad),
+            (self.card_ad, self.card_kh),
+            (self.card_kh, self.card_kh),
+            (self.card_kh, self.card_qc),
+            (self.card_qc, self.card_qc),
+            (self.card_qc, self.card_js),
+            (self.card_js, self.card_js),
+            (self.card_js, self.card_2s),
+        )
+
+        for instance_a, instance_b in test_cases:
+            with self.subTest(f"{instance_a} >= {instance_b}"):
+                self.assertTrue(instance_a >= instance_b)
 
     def test_card_lt(self):
-        self.assertTrue(self.card_2s < self.card_js)
-        self.assertTrue(self.card_js < self.card_qc)
-        self.assertTrue(self.card_qc < self.card_kh)
-        self.assertTrue(self.card_kh < self.card_ad)
+        test_cases = (
+            (self.card_2s, self.card_js),
+            (self.card_js, self.card_qc),
+            (self.card_qc, self.card_kh),
+            (self.card_kh, self.card_ad),
+        )
+
+        for instance_a, instance_b in test_cases:
+            with self.subTest(f"{instance_a} < {instance_b}"):
+                self.assertTrue(instance_a < instance_b)
 
     def test_card_le(self):
         self.assertTrue(self.card_2s <= self.card_2s)
@@ -60,47 +89,52 @@ class testCard(unittest.TestCase):
         self.assertTrue(self.card_qc <= self.card_kh)
         self.assertTrue(self.card_kh <= self.card_kh)
         self.assertTrue(self.card_kh <= self.card_ad)
+        test_cases = (
+            (self.card_2s, self.card_2s),
+            (self.card_2s, self.card_js),
+            (self.card_js, self.card_js),
+            (self.card_js, self.card_qc),
+            (self.card_qc, self.card_qc),
+            (self.card_qc, self.card_kh),
+            (self.card_kh, self.card_kh),
+            (self.card_kh, self.card_ad),
+        )
+
+        for instance_a, instance_b in test_cases:
+            with self.subTest(f"{instance_a} <= {instance_b}"):
+                self.assertTrue(instance_a <= instance_b)
 
     def test_card_ne(self):
-        self.assertTrue(self.card_2s != self.card_js)
-        self.assertTrue(self.card_js != self.card_qc)
-        self.assertTrue(self.card_qc != self.card_kh)
-        self.assertTrue(self.card_kh != self.card_ad)
+        test_cases = (
+            (self.card_2s, self.card_js),
+            (self.card_js, self.card_qc),
+            (self.card_qc, self.card_kh),
+            (self.card_kh, self.card_ad),
+        )
+
+        for instance_a, instance_b in test_cases:
+            with self.subTest(f"{instance_a} != {instance_b}"):
+                self.assertTrue(instance_a != instance_b)
 
     def test_card_eq(self):
-        my_card_2d = Card(Rank(2), Suit.D)
-        my_card_jd = Card(Rank(11), Suit.D)
-        my_card_qh = Card(Rank(12), Suit.H)
-        my_card_kc = Card(Rank(13), Suit.C)
-        my_card_as = Card(Rank(14), Suit.S)
+        test_cases = (
+            (self.card_2s, Card(Rank(2), Suit.D)),
+            (self.card_js, Card(Rank(11), Suit.D)),
+            (self.card_qc, Card(Rank(12), Suit.H)),
+            (self.card_kh, Card(Rank(13), Suit.C)),
+            (self.card_ad, Card(Rank(14), Suit.S)),
+        )
 
-        self.assertTrue(self.card_2s == my_card_2d)
-        self.assertTrue(self.card_js == my_card_jd)
-        self.assertTrue(self.card_qc == my_card_qh)
-        self.assertTrue(self.card_kh == my_card_kc)
-        self.assertTrue(self.card_ad == my_card_as)
+        for instance_a, instance_b in test_cases:
+            with self.subTest(f"{instance_a} == {instance_b}"):
+                self.assertTrue(instance_a == instance_b)
 
     def test_construct_from_string(self):
-        card_2s_str = '2S'
-        card_js_str = 'JS'
-        card_qc_str = 'QC'
-        card_kh_str = 'KH'
-        card_ad_str = 'AD'
-        card_td_str = '10D'
+        test_cases = ('2S', 'JS', 'QC', 'KH', 'AD', '10D')
 
-        card_2s = Card(card_2s_str)
-        card_js = Card(card_js_str)
-        card_qc = Card(card_qc_str)
-        card_kh = Card(card_kh_str)
-        card_ad = Card(card_ad_str)
-        card_td = Card(card_td_str)
-
-        self.assertEqual(card_2s_str, repr(card_2s))
-        self.assertEqual(card_js_str, repr(card_js))
-        self.assertEqual(card_qc_str, repr(card_qc))
-        self.assertEqual(card_kh_str, repr(card_kh))
-        self.assertEqual(card_ad_str, repr(card_ad))
-        self.assertEqual(card_td_str, repr(card_td))
+        for init_str in test_cases:
+            with self.subTest(f"Card(\"{init_str}\")"):
+                self.assertTrue(init_str == repr(Card(init_str)))
 
     # noinspection PyUnusedLocal
     def test_bad_card_string_rank_1(self):
