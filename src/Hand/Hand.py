d005058ef82746fd9f7d8311948ada9ab21cc208
David Cox <dave@codavex.co.uk>
2022-12-27 15:11:42 +0000

    build hand from string

--- src/Hand/Hand.py
+++ src/Hand/Hand.py
@@ -1,6 +1,21 @@
+from src.Card.Card import Card
+
+
 class Hand(list):
     def size(self):
         return len(self)
 
     def draw(self):
         return super().pop(0)
+
+    def build_from_str(self, hand_as_str: str) -> object:
+        # to build a hand from a string depicting a hand
+        # eg "[AS, AV, AH, AD, kS]"
+        self.clear()
+        hand_as_str = hand_as_str.removeprefix("[")
+        hand_as_str = hand_as_str.removesuffix("]")
+        hand_as_str = hand_as_str.replace(" ", "")
+        cards = hand_as_str.split(',')
+        for card in cards:
+            self.append(Card(card))
+        return
--- test/Hand/testHand.py
+++ test/Hand/testHand.py
@@ -26,6 +26,23 @@ class testHand(unittest.TestCase):
         self.assertEqual(2, hand.size())
         self.assertEqual("[AS, AC]", str(hand))
 
+    def test_build_from_str(self):
+        test_cases = (
+            "[AS, AC, AH, AD, 2D]",
+            "[KH, KD, KS, AS, AC]",
+            "[AS, 7S, 5S, 3S, 2S]",
+            "[9S, 8C, 7H, 6D, 5D]",
+            "[5C, 4H, 3D, 2D, AS]",
+            "[AS, KH, QD, JD, 10C]",
+            "[AS, AC, AH, 3D, 2D]"
+        )
+
+        for case in test_cases:
+            with self.subTest(f"build {case}"):
+                hand = Hand()
+                hand.build_from_str(case)
+                self.assertEqual(case, str(hand))
+
 
 if __name__ == '__main__':
     unittest.main()
